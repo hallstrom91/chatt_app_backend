9da7fc4b52c3cb81959fcb569d12bec89586e47f
hallstrom91 <simonhallstrom@live.se>
2024-09-09 13:02:30 +0200

    update routes for user

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const {
   createUser,
+  loginUser,
   getAllUsers,
   getUserById,
+  updateUser,
+  deleteUser,
 } = require("../controllers/userController");
+const { validateCSRFToken, generateCSRFToken } = require("../utils/csrfUtils");
+const { validateJwtToken } = require("../utils/jwtUtils");
 
+// express router
 const router = express.Router();
 
-router.post("/register", createUser);
-router.get("/", getAllUsers);
-router.get("/:userId", getUserById);
+/*
+=====================================
+CSRF PATCH & CSRF REQUIRED ROUTES
+=====================================
+*/
+router.patch("/csrf", generateCSRFToken);
+router.post("/auth/register", validateCSRFToken, createUser);
+router.post("/auth/token", validateCSRFToken, loginUser);
+
+/*
+=====================================
+JWT REQUIRED USER ROUTES
+=====================================
+*/
+router.post("/invite/:userId", validateJwtToken);
+router.get("/users", validateJwtToken, getAllUsers);
+router.get("/users/:userId", validateJwtToken, getUserById);
+router.put("/user", validateJwtToken, updateUser);
+router.delete("/users/:userId", validateJwtToken, deleteUser);
 
 module.exports = router;
