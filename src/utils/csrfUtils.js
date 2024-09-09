3f8923e3604a9394f224a6aa13530046f2ee4420
hallstrom91 <simonhallstrom@live.se>
2024-09-09 13:04:01 +0200

    Add generateCSRFToken and validateCSRFToken functions

---
+++ src/utils/csrfUtils.js
@@ -0,0 +1,30 @@
+const crypto = require("crypto");
+
+/*
+=====================================
+GENERATE CSRF TOKEN FUNCTION
+=====================================
+*/
+const generateCSRFToken = (req, res) => {
+  const csrfToken = crypto.randomBytes(32).toString("hex");
+  res.app.locals.csrfToken = csrfToken;
+  return res.json({ csrfToken });
+};
+
+/*
+=====================================
+VALIDATE CSRF TOKEN FUNCTION
+=====================================
+*/
+const validateCSRFToken = (req, res, next) => {
+  // change 2 correct
+  const csrfToken = req.body.csrfToken || req.headers["x-csrf-token"];
+
+  if (csrfToken && csrfToken === req.app.locals.csrfToken) {
+    return next();
+  }
+
+  return res.status(403).json({ message: "Invalid or missing CSRF token" });
+};
+
+module.exports = { generateCSRFToken, validateCSRFToken };
