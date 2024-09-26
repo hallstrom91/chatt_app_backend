57548b0d2d4cedd73184d69db1061551659cc3d3
hallstrom91 <simonhallstrom@live.se>
2024-09-26 23:39:57 +0200

    added connection for socketIo server, add auth routes, add invite routes, updated cors-options and added Sentry error handler

--- src/server.js
+++ src/server.js
@@ -1,22 +1,31 @@
+require("./utils/instrument.js");
 require("dotenv").config({ path: [".env.development.local", ".env"] });
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const { connectDB } = require("./utils/mongoDB");
+const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const messageRoutes = require("./routes/messageRoutes");
-const { generateCSRFToken } = require("./utils/csrfUtils");
+const inviteRoutes = require("./routes/inviteRoutes");
+const Sentry = require("@sentry/node");
+
+//socketio test
+const { socketIoSetup } = require("./utils/socketIo");
+const http = require("node:http");
 
 const PORT = process.env.PORT;
 const app = express();
+const server = http.createServer(app);
 
 connectDB().catch((err) => console.error("DB connection error", err));
 
 const corsOptions = {
   credentials: true,
+
   origin: ["http://localhost:3000", "http://localhost:5173"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization", "Baggage", "Sentry-trace"],
 };
 
 app.use(
@@ -37,9 +46,16 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // routes
+app.use("/", authRoutes);
 app.use("/", userRoutes);
 app.use("/", messageRoutes);
+app.use("/", inviteRoutes);
 
-app.listen(PORT, () => {
+//use socket
+socketIoSetup(server);
+
+Sentry.setupExpressErrorHandler(app);
+
+server.listen(PORT, () => {
   console.log(`Server is live on port: ${PORT}`);
 });
