e8fd943b8bd5f985686719c53f647b392c5ad053
hallstrom91 <simonhallstrom@live.se>
2024-09-26 23:37:02 +0200

    add Sentry error logger

---
+++ src/utils/instrument.js
@@ -0,0 +1,11 @@
+require("dotenv").config({ path: [".env.development.local", ".env"] });
+const Sentry = require("@sentry/node");
+const { nodeProfilingIntegration } = require("@sentry/profiling-node");
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [nodeProfilingIntegration()],
+  // Tracing
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+});
